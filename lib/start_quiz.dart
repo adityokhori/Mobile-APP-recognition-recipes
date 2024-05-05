9535dc8ee68c24e676ac2a2835b8babd5c74e89b
adityokhori <adityo.khori@gmail.com>
2024-05-05 19:40:11 +0700

    input poin beres

--- lib/start_quiz.dart
+++ lib/start_quiz.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:grinv/open_bottom.dart';
+import 'dart:math';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class StartQuiz extends StatelessWidget {
   @override
@@ -31,17 +33,15 @@ class StartQuiz extends StatelessWidget {
             );
           }
 
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (BuildContext context, int index) {
-              var questionData = snapshot.data!.docs[index];
-              return QuizQuestionCard(
-                question: questionData['question'],
-                options: List<String>.from(questionData['options']),
-                timer: questionData['timer'],
-                correctAnswer: questionData['correctAnswer'],
-              );
-            },
+          var randomQuestionData =
+              snapshot.data!.docs[Random().nextInt(snapshot.data!.docs.length)];
+
+          return QuizQuestionCard(
+            question: randomQuestionData['question'],
+            options: List<String>.from(randomQuestionData['options']),
+            timer: randomQuestionData['timer'],
+            correctAnswer: randomQuestionData['correctAnswer'],
+            point: randomQuestionData['point'],
           );
         },
       ),
@@ -54,12 +54,14 @@ class QuizQuestionCard extends StatefulWidget {
   final List<String> options;
   final int timer;
   final String correctAnswer;
+  final int point;
 
   QuizQuestionCard(
       {required this.question,
       required this.options,
       required this.timer,
-      required this.correctAnswer});
+      required this.correctAnswer,
+      required this.point});
 
   @override
   _QuizQuestionCardState createState() => _QuizQuestionCardState();
@@ -145,7 +147,7 @@ class _QuizQuestionCardState extends State<QuizQuestionCard> {
   }
 
   void _checkAnswer() {
-    _timer.cancel(); // Stop the timer when submitting answer
+    _timer.cancel();
     List<String> selectedOptions = [];
     for (int i = 0; i < _isChecked.length; i++) {
       if (_isChecked[i]) {
@@ -154,11 +156,39 @@ class _QuizQuestionCardState extends State<QuizQuestionCard> {
     }
 
     bool isCorrect = selectedOptions.contains(widget.correctAnswer);
-
+    int point = isCorrect ? widget.point : 0;
+    
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(isCorrect ? 'Correct Answer!' : 'Wrong Answer!'),
+      content: Text(isCorrect ? 'Correct Answer!, You got $point point' : 'Wrong Answer!'),
+      backgroundColor: isCorrect ? Colors.green : Colors.red,
     ));
+ // Jika jawaban benar, tambahkan poin ke data pengguna di Firebase
+  if (isCorrect) {
+    _updateUserPoints(point);
   }
+}
+
+void _updateUserPoints(int pointsToAdd) async {
+  try {
+    final user = FirebaseAuth.instance.currentUser;
+    final userId = user?.uid;
+    var userDoc = FirebaseFirestore.instance.collection('users').doc(userId);
+    
+    var userData = await userDoc.get();
+    
+    if (userData.exists && userData.data()!.containsKey('point_quiz')) {
+      int currentPoints = userData['point_quiz'];
+      int updatedPoints = currentPoints + pointsToAdd;
+      
+      await userDoc.update({'point_quiz': updatedPoints});
+    } else {
+      await userDoc.set({'point_quiz': pointsToAdd}, SetOptions(merge: true));
+    }
+  } catch (error) {
+    print('Error updating user points: $error');
+  }
+}
+
 
   void _showTimeUpDialog() {
     showDialog(
@@ -172,11 +202,11 @@ class _QuizQuestionCardState extends State<QuizQuestionCard> {
               child: Text("OK"),
               onPressed: () {
                 Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => OpenButtom(),
-                      ),
-                    );
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OpenButtom(),
+                  ),
+                );
               },
             ),
           ],
